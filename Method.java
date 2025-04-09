fea3e7c0a71ba7386daad96ede7c4028041c481a
shreyas801 <shreyastotagi143@gmail.com>
2025-04-09 16:00:09 +0530

    Method overloading

---
+++ Method.java
@@ -0,0 +1,26 @@
+public class Method {
+    void display(){
+        System.out.println("Value of a = 0");
+        System.out.println("Value of b = 0");
+    }
+
+    void display(int a){
+        System.out.println("Value of a = "+a);
+        System.out.println("Value of b = 0");
+    }
+
+    void display(int a,int b){
+        System.out.println("Value of a = "+a);
+        System.out.println("Value of b = "+b);
+    }
+
+    public static void main(String[] args)
+    {
+        Method m = new Method();
+        m.display();
+        m.display(5);
+        m.display(5, 10);
+    }
+
+    
+}
